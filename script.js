ea77e4f2245bb26b135d7e90ac75f766a4529e2e
vaibhav chauhan <vishuchauhan461@gmail.com>
2024-09-26 23:58:56 +0530

    Able to add and fetch out the word to be deleted easily from the code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const url = `https://random-word-api.herokuapp.com/all`
 var words = Array();
 var shuffledWords = Array(); // used to store the shuffled array of words
 var choiceArray = Array();
-var selectedWords=Array();
+var selectedWords = Array();
 
 
 async function fetchAllWord() {
@@ -16,7 +16,7 @@ async function fetchAllWord() {
             let data = await response.json();
             console.log(data);
             words = data;
-            words = words.splice(0,25); // storing only first 25 elements 
+            words = words.splice(0, 25); // storing only first 25 elements 
             console.log("the reduced array is");
             console.log(words)
         }
@@ -59,13 +59,15 @@ function generate3RandomChoice() {
     return Array.from(randomChoices);
 }
 
-  
+
 // Making the array shuffle and starting the Game : in this array has to be shuffled and generation of the random words for score Checking and also 
 
 function startGame() {
     //using shuffled array as original here 
     console.log(" Starting the Game ");
     shuffledWords = shuffleArray(words);
+    selectedWords = []; // making the selectetion as zero each time game start
+    choiceArray = []; // making the array which contain random 3 choices to be made empty
     console.log(shuffledWords);
     choiceArray = generate3RandomChoice();
     console.log("choices are as " + choiceArray);
@@ -77,33 +79,84 @@ function startGame() {
                 <button id="cross-${i}" class="hidden">X</button>
             </div>`
     }
-    
     // showing the generated choice
-    let i=0;
-    while(i<3)
-    {
-        document.getElementById(`selectedWord-${i+1}`).innerHTML=choiceArray[i];
+    let i = 0;
+    while (i < 3) {
+        document.getElementById(`selectedWord-${i + 1}`).innerHTML = choiceArray[i];
         i++;
     }
 
     // MAKING USER TO SELECT THE CHOICE 
-    if(selectedWords.length==3)
-    {
-        alert("You have fullfiled your choices already")
-    }
-    else{
+    for (let i = 0; i < shuffledWords.length; i++) {
         document.getElementById(`word-${i}`).addEventListener("click", () => {
-            console.log(`hello Button clicked successfully ${shuffledWords[i]}`)
-            selectedWords.push(shuffledWords[i]);
+            console.log(`The selected word from the user is as:  ${shuffledWords[i]}`)
+            handleClickEvent(shuffledWords[i]);
+            console.log("handle click function invoked with : " + shuffledWords[i]);
 
+            // Enabling the X sign to show up in the word also 
+
+            let cancel=document.getElementById(`cross-${i}`);
+            cancel.classList.remove('hidden');
         })
     }
-    // for (let i = 0; i < words.length; i++) {
-    //     document.getElementById(`word-${i}`).addEventListener("click", () => {
-    //         console.log(`hello Button clicked successfully ${words[i]}`)
-
-    //     })
-    // } 
-
+    for(let i=0;i<shuffledWords.length;i++)
+    {
+        document.getElementById(`cross-${i}`).addEventListener("click",()=>
+        {
+            console.log("Enable the X sign in that name of the word to be delete "+shuffledWords[i]);
+            deleteSelection(shuffledWords[i]);
+        })
+    }
 }
-// handling of the selected word has to be called out again as it was not working well : implement onclick with function or fetch element from Dom lets think of it :
+
+function handleClickEvent(userchoice) {
+    if (selectedWords.length >= 3) {
+        alert("You have made all your choices and not allowed to make more of it ");
+        return;
+
+    }
+
+    else {
+        if (!selectedWords.includes(userchoice)) {
+            console.log("User choice is :" + userchoice)
+            selectedWords.push(userchoice);
+            displaySelectedWord();
+        }
+        else {
+            // alert("The Word is already selected make a different selection please");
+        }
+    }
+}
+
+function displaySelectedWord() {
+    for (let i = 0; i < selectedWords.length; i++) {
+        document.getElementById(`selectedWord-${i + 1}`).innerHTML = selectedWords[i];
+    }
+}
+
+// function deleteSelection(wordToBeDeleted)
+// {
+//     console.log("deletion word ")
+//     console.log(wordToBeDeleted);
+//     let indexToBeDeleted=selectedWords.indexOf(wordToBeDeleted);
+//     console.log("deleted index is :"+indexToBeDeleted);
+//     selectedWords.splice(indexToBeDeleted,1);
+//     displaySelectedWord();
+
+// }
+
+function deleteSelection(wordToBeDeleted) {
+    console.log("Attempting to delete word: ", wordToBeDeleted);
+
+    // Find the index of the word to be deleted in the array
+    let indexToBeDeleted = selectedWords.indexOf(wordToBeDeleted);
+
+    // Check if the word exists in the array
+    if (indexToBeDeleted !== -1) {
+        console.log("Deleted index is: " + indexToBeDeleted);
+        selectedWords.splice(indexToBeDeleted, 1); // Remove the word from the array
+        displaySelectedWord(); // Update the display after deletion
+    } else {
+        console.log("Word not found in the selectedWords array.");
+    }
+}
